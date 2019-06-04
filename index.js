ec136581dec7d47b42563aa6c7f1eafaed08bf5e
David Gasser <gasserdav@gmail.com>
2019-06-04 14:14:38 +0200

    Simple server

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("SERVER LAUNCHED")
+
+var express = require('express')
+var cors = require('cors')
+var path = require('path')
+
+var app = express()
+
+app.options('*', cors()) // include before other routes 
+app.use(cors())
+
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    next()
+})
+
+// Use app.get as app.use === middlware (Module erstellen)
+
+
+app.use(express.static('public'))
+
+// viewed at http://localhost:8080
+app.get('/', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log(`\nPEM Backend started...\n`)
+    console.log(`️⚔️  BACKEND listening on port ${port} ⚔️`)
+
+})
