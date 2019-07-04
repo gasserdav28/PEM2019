3d538f14443343b04cf3f023fd77507cd5c7ed9b
alexanderperzl <alexander.perzl@gmx.de>
2019-07-04 22:53:13 +0200

    Extracts sensorData model to file Simplifies POST /sensorData: sensor points in array are automatically saved as single documents to the database

--- src/endpoints/sensorData.js
+++ src/endpoints/sensorData.js
@@ -1,48 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-
-
-// versuche das mal in ein eigenes file zu stecken
-
-var sensorSchema = new mongoose.Schema({
-    user_id: Number,
-    created: { type: Date, default: Date.now },
-    data: Object,
-})
-
-
+const express = require('express');
+const router = express.Router();
+var SensorData = require('../model/sensorData');
 
 router.get('/', function (req, res) {
-    let dataType = req.query.dataType
-    let from = req.query.from
-    let to = req.query.to
+    let dataType = req.query.dataType;
+    let from = req.query.from;
+    let to = req.query.to;
 
     // query database
     // send response data
     res.send(" - " + dataType + from + to)
-})
+});
 
-
-// speichert gerade nur 1 objekt. kann man aber erweitern. dann nehmen wir einfach saveMany und lassen immer einen array schicken?
+// Saves an array of sensor data as single entries to the database
 router.post('/', function (req, res) {
+    console.log(req.body);
+    SensorData.create(req.body, function (err) {
+        if (err) return console.error(err);
+        res.json({ success: true });
+    });
+});
 
-    var dataModel = mongoose.model('Sensor', sensorSchema, 'sensordata')
-
-    //sollte als array kommen!
-    var newData = req.body.data
-    // a document instance
-    var data = []
-    data.push(new dataModel({ user_id: 123, data: null }))
-
-    // save model to database
-    dataModel.insertMany(data, function (err, data) {
-        if (err) return console.error(err)
-        console.log('Data for user: ' + data[0].user_id + " saved to database.")
-    })
-    // transform
-    // save to database
-    res.json({ sucess: true })
-})
-
-module.exports = router
+module.exports = router;
--- src/model/sensorData.js
+++ src/model/sensorData.js
@@ -1,15 +1,9 @@
-
 const mongoose = require('mongoose');
 
 var sensorSchema = mongoose.Schema({
-    user_id: Number,
+    userId: Number,
     created: { type: Date, default: Date.now },
-    data: Schema.Types.Mixed,
-})
+    data: mongoose.Schema.Types.Mixed,
+});
 
-sensorSchema.statics.safeSensorData = function safeSensorData(){}
-
-
-module.exports = {
-    Sensor: mongoose.model('Sensor', sensorSchema, 'sensordata'),
-};
+module.exports = mongoose.model('SensorData', sensorSchema, 'sensordata');
