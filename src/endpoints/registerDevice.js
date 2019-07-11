a4b1c5d2a5e2f079d67ce21c275143b1339440c7
alexanderperzl <alexander.perzl@gmx.de>
2019-07-11 19:32:58 +0200

    Fixes creation of userId

--- src/endpoints/registerDevice.js
+++ src/endpoints/registerDevice.js
@@ -7,11 +7,11 @@ router.get('/', function (req, res) {
     // TODO detect if id already in use
     let userId = cryptoRandomString({length: 7, characters: '1234567890'});
 
-    var newUser = new User({id: userId});
+    var newUser = new User({userId: userId});
 
     newUser.save(function (err, user) {
         if (err) return console.error(err);
-        res.json({userId: user.id});
+        res.json({userId: user.userId});
     });
 });
 
