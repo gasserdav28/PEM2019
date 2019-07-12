4023af4936899ff88b490555f22fdfa598432558
David Gasser <gasserdav@gmail.com>
2019-07-12 16:14:37 +0200

    Parse time from epoch to UTC

--- src/endpoints/sensorData.js
+++ src/endpoints/sensorData.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var SensorData = require('../model/sensorData');
 let authentication = require('../authentication');
+var moment = require('moment');
 
 router.get('/', authentication.authentication, function (req, res) {
     let sensorId = req.query.sensorId;
@@ -10,19 +11,20 @@ router.get('/', authentication.authentication, function (req, res) {
     let userId = req.userId;
     console.log(userId);
 
-    if (sensorId === undefined) {
-        return res.status(400).send({code: 1, msg:'Missing query parameter: sensorId'})
+    if (!sensorId) {
+        return res.status(400).send({ code: 1, msg: 'Missing query parameter: sensorId' })
     }
 
-    if (userId === undefined) {
-        return res.status(400).send({code: 1, msg:'Missing query parameter: userId'})
+    if (!userId) {
+        return res.status(400).send({ code: 1, msg: 'Missing query parameter: userId' })
     }
 
     // TODO: add from and to
-    SensorData.find({ userId: userId, sensorId: sensorId }  , function (err, data) {
+    SensorData.find({ userId: userId, sensorId: sensorId }, function (err, data) {
+
         if (err) {
             console.error(err);
-            return res.status(400).send({code: 2, msg: err});
+            return res.status(400).send({ code: 2, msg: err });
         }
         return res.json(data);
     });
@@ -30,16 +32,29 @@ router.get('/', authentication.authentication, function (req, res) {
 
 // Saves an array of sensor data as single entries to the database
 router.post('/', function (req, res) {
+
+    // Parses timestamp to UTC (array)
+    // req.body.forEach(element => {
+    //     let time = parseInt(element.timestamp)
+    //     element.timestamp = moment(time).format()
+    // });
+
+     // Parses timestamp to UTC (single Obj)
+    let time = parseInt(req.body.timestamp)
+    body.timestamp = moment(time).format()
+
+    // Validate UserId
     SensorData.create(req.body, function (err) {
         if (err) {
             console.error(err);
-            return res.status(400).send({code: 3, msg: err});
+            return res.status(400).send({ code: 3, msg: err });
         }
         return res.json({ success: true });
     });
 });
 
 router.get('/sensorIds', function (req, res) {
+
     var sensors = {
         'Sensor1': {
             id: '1',
